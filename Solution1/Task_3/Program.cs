b36fc835afc405c11c4f7d580fda15299a29417e
VladKrivonosov <wonin08@gmail.com>
2020-04-19 14:10:57 +0300

    all task done

---
+++ Solution1/Task_3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder str = new StringBuilder("1a!2.3!!.. 4.!.?6 7! ..?");
+
+            Console.WriteLine($"Исхлдная строка: {str}");
+
+            int i = 0;
+            while (str[i] != '?')
+            {
+                if (str[i] == '!' || str[i] == '.')
+                {
+                    str.Remove(i, 1);
+                    --i;
+                }
+                i++;
+            }
+
+            int numberID = 0;
+            for (int j = 0; j < str.Length; j++)
+            {
+                if (str[j]=='?')
+                {
+                    numberID = i;
+                    break;
+                }
+            }
+
+            for (int k = numberID; k < str.Length; k++)
+            {
+                if (char.IsWhiteSpace(str[k]))
+                {
+                    str.Replace(str[k], '_',k,1);
+                }
+            }
+            Console.WriteLine($"Форматированная строка: {str}");
+        }
+    }
+}
